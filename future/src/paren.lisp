9cd8b29d3cd7e7e5d56da026203a8e7c98b0717a
jcguu95 <jcguu95@gmail.com>
2024-05-06 09:26:38 -0400

    Rewrite c op defs in macros.

--- future/src/paren.lisp
+++ future/src/paren.lisp
@@ -18,19 +18,6 @@
             (let ((function (gethash operator *functions*)))
               (funcall function (cdr form)))))))
 
-(setf (gethash 'deftype *functions*)
-      (lambda (form)
-        (let ((type (nth 0 form))
-              (new-type (nth 1 form)))
-          (format nil "typedef ~a ~a;" type new-type))))
-
-(setf (gethash 'defstruct *functions*)
-      (lambda (form)
-        (let ((struct-name (nth 0 form))
-              (cells (nth 1 form)))
-          (format nil "struct ~a {~%~{  ~a;~^~%~}~%};"
-                  struct-name (mapcar #'resolve-declaration cells)))))
-
 (defun resolve-declaration (declaration)
   (let ((variable (nth 0 declaration)))
     (multiple-value-bind (type pointer-count)
@@ -53,53 +40,40 @@
                  1)))
     (t (error "Unsupported case."))))
 
-(setf (gethash '-> *functions*)
-      (lambda (form)
-        (format nil "~{~a~^->~}" form)))
+(defmacro def-cop (name _ &body body)
+  "Define a C operation."
+  ;; FIXME FORM is a special keyword here. This is ugly. What should we do?
+  (declare (ignore _))
+  `(setf (gethash ',name *functions*)
+         (lambda (form) ,@body)))
 
-(setf (gethash 'str *functions*)
-      (lambda (form)
-        (format nil "~s" (car form))))
+(def-cop deftype ()
+  (let ((type (nth 0 form))
+        (new-type (nth 1 form)))
+    (format nil "typedef ~a ~a;" type new-type)))
 
-(setf (gethash '== *functions*)
-      (lambda (form)
-        (format nil "(~a == ~a)" (c (nth 0 form)) (c (nth 1 form)))))
+(def-cop defstruct ()
+  (let ((struct-name (nth 0 form))
+        (cells (nth 1 form)))
+    (format nil "struct ~a {~%~{  ~a;~^~%~}~%};"
+            struct-name (mapcar #'resolve-declaration cells))))
 
-(setf (gethash '> *functions*)
-      (lambda (form)
-        (format nil "(~a > ~a)" (c (nth 0 form)) (c (nth 1 form)))))
+(def-cop defun ()
+  (let ((func-name (nth 0 form))
+        (arguments (nth 1 form))
+        (body (cddr form)))
+    (format nil "~a (~{~a~^, ~}) {~%~{  ~a;~%~}}"
+            (resolve-declaration func-name)
+            (mapcar #'resolve-declaration arguments)
+            (mapcar #'c body))))
 
-(setf (gethash '< *functions*)
-      (lambda (form)
-        (format nil "(~a < ~a)" (c (nth 0 form)) (c (nth 1 form)))))
-
-(setf (gethash 'or *functions*)
-      (lambda (form)
-        (format nil "(~a || ~a)" (c (nth 0 form)) (c (nth 1 form)))))
-
-(setf (gethash 'and *functions*)
-      (lambda (form)
-        (format nil "(~a && ~a)" (c (nth 0 form)) (c (nth 1 form)))))
-
-;; TODO Implement include for local libs too.
-(setf (gethash 'include *functions*)
-      (lambda (form)
-        (format nil "#include <~a>" (car form))))
-
-(setf (gethash 'defun *functions*)
-      (lambda (form)
-        (let ((func-name (nth 0 form))
-              (arguments (nth 1 form))
-              (body (cddr form)))
-          (format nil "~a (~{~a~^, ~}) {~%~{  ~a;~%~}}"
-                  (resolve-declaration func-name)
-                  (mapcar #'resolve-declaration arguments)
-                  (mapcar #'c body)))))
-
-(setf (gethash 'return *functions*)
-      (lambda (form)
-        (format nil "return ~a" (car form))))
-
-(setf (gethash 'set *functions*)
-      (lambda (form)
-        (format nil "~a = ~a" (resolve-declaration (nth 0 form)) (c (nth 1 form)))))
+(def-cop ->  () (format nil "~{~a~^->~}" form))
+(def-cop ==  () (format nil "(~a == ~a)" (c (nth 0 form)) (c (nth 1 form))))
+(def-cop >   () (format nil "(~a > ~a)" (c (nth 0 form)) (c (nth 1 form))))
+(def-cop <   () (format nil "(~a < ~a)" (c (nth 0 form)) (c (nth 1 form))))
+(def-cop or  () (format nil "(~a || ~a)" (c (nth 0 form)) (c (nth 1 form))))
+(def-cop and () (format nil "(~a && ~a)" (c (nth 0 form)) (c (nth 1 form))))
+(def-cop set () (format nil "~a = ~a" (resolve-declaration (nth 0 form)) (c (nth 1 form))))
+(def-cop str () (format nil "~s" (car form)))
+(def-cop return  () (format nil "return ~a" (car form)))
+(def-cop include () (format nil "#include <~a>" (car form)))
