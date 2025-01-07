f8b2100cb4c315169ea56c9221fbfe697de31e93
jcguu95 <jcguu95@gmail.com>
2025-01-07 10:19:57 -0700

    Fixed all issues in sectorlisp.lsp!
    All tests passed!

--- future/test/paren.test.lisp
+++ future/test/paren.test.lisp
@@ -1301,19 +1301,79 @@ x1->next->next->value = 30
                    "Enter an integer for i: Wrong guess. Aborting..~%"
                    :expected-stderr ""))
 
+
+  ;; Sector Lisp
+
+  (is
+   (test-c-program "../examples/sectorlisp/lisp.c"
+                   :stdin "(QUOTE A) "
+                   :expected-stdout "A~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "(QUOTE A) "
+                   :expected-stdout "A~%"))
+
+  (is
+   (test-c-program "../examples/sectorlisp/lisp.c"
+                   :stdin "(QUOTE (QUOTE A)) "
+                   :expected-stdout "(QUOTE A)~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "(QUOTE (QUOTE A)) "
+                   :expected-stdout "(QUOTE A)~%"))
+
+  (is
+   (test-c-program "../examples/sectorlisp/lisp.c"
+                   :stdin "(QUOTE (+ 1 2 3)) "
+                   :expected-stdout "(+ 1 2 3)~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "(QUOTE (+ 1 2 3)) "
+                   :expected-stdout "(+ 1 2 3)~%"))
+
+  (is
+   (test-c-program "../examples/sectorlisp/lisp.c"
+                   :stdin "(EQ (QUOTE A) (QUOTE A)) "
+                   :expected-stdout "T~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "(EQ (QUOTE A) (QUOTE A)) "
+                   :expected-stdout "T~%"))
+
   (is
    (test-c-program "../examples/sectorlisp/lisp.c"
                    :stdin "(EQ (QUOTE T) (QUOTE F)) "
                    :expected-stdout "NIL~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "(EQ (QUOTE T) (QUOTE F)) "
+                   :expected-stdout "NIL~%"))
+
   (is
    (test-c-program "../examples/sectorlisp/lisp.c"
                    :stdin "
   (EQ (QUOTE T) (QUOTE F))
   (EQ (QUOTE A) (QUOTE A)) "
                    :expected-stdout "NIL~%T~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
+                   :stdin "
+  (EQ (QUOTE T) (QUOTE F))
+  (EQ (QUOTE A) (QUOTE A)) "
+                   :expected-stdout "NIL~%T~%"))
+
 
   (is
    (test-c-program "../examples/sectorlisp/lisp.c"
+                   :stdin "
+  ((LAMBDA (FF X) (FF X))
+   (QUOTE (LAMBDA (X)
+            (COND ((ATOM X) X)
+                  ((QUOTE T) (FF (CAR X))))))
+   (QUOTE ((A) B C))) "
+                   :expected-stdout "A~%"))
+  (is
+   (test-c-program "../examples/sectorlisp.c"
                    :stdin "
   ((LAMBDA (FF X) (FF X))
    (QUOTE (LAMBDA (X)
@@ -1371,11 +1431,54 @@ x1->next->next->value = 30
                      ((QUOTE T) (APPLY (CAR E) (EVLIS (CDR E) A) A))))
               ((QUOTE T) (APPLY (CAR E) (EVLIS (CDR E) A) A)))))) "
                    :expected-stdout "A~%"))
-
-  ;; FIXME We are working on sectorlisp.c.
-  ;;
   (is
    (test-c-program "../examples/sectorlisp.c"
-                   :stdin "(A A)"
+                   :stdin "
+  ((LAMBDA (ASSOC EVCON PAIRLIS EVLIS APPLY EVAL)
+     (EVAL (QUOTE ((LAMBDA (FF X) (FF X))
+                   (QUOTE (LAMBDA (X)
+                            (COND ((ATOM X) X)
+                                  ((QUOTE T) (FF (CAR X))))))
+                   (QUOTE ((A) B C))))
+           ()))
+   (QUOTE (LAMBDA (X Y)
+            (COND ((EQ Y ()) ())
+                  ((EQ X (CAR (CAR Y)))
+                         (CDR (CAR Y)))
+                  ((QUOTE T)
+                   (ASSOC X (CDR Y))))))
+   (QUOTE (LAMBDA (C A)
+            (COND ((EVAL (CAR (CAR C)) A)
+                   (EVAL (CAR (CDR (CAR C))) A))
+                  ((QUOTE T) (EVCON (CDR C) A)))))
+   (QUOTE (LAMBDA (X Y A)
+            (COND ((EQ X ()) A)
+                  ((QUOTE T) (CONS (CONS (CAR X) (CAR Y))
+                                   (PAIRLIS (CDR X) (CDR Y) A))))))
+   (QUOTE (LAMBDA (M A)
+            (COND ((EQ M ()) ())
+                  ((QUOTE T) (CONS (EVAL (CAR M) A)
+                                   (EVLIS (CDR M) A))))))
+   (QUOTE (LAMBDA (FN X A)
+            (COND
+              ((ATOM FN)
+               (COND ((EQ FN (QUOTE CAR))  (CAR  (CAR X)))
+                     ((EQ FN (QUOTE CDR))  (CDR  (CAR X)))
+                     ((EQ FN (QUOTE ATOM)) (ATOM (CAR X)))
+                     ((EQ FN (QUOTE CONS)) (CONS (CAR X) (CAR (CDR X))))
+                     ((EQ FN (QUOTE EQ))   (EQ   (CAR X) (CAR (CDR X))))
+                     ((QUOTE T)            (APPLY (EVAL FN A) X A))))
+              ((EQ (CAR FN) (QUOTE LAMBDA))
+               (EVAL (CAR (CDR (CDR FN)))
+                     (PAIRLIS (CAR (CDR FN)) X A))))))
+   (QUOTE (LAMBDA (E A)
+            (COND
+              ((ATOM E) (ASSOC E A))
+              ((ATOM (CAR E))
+               (COND ((EQ (CAR E) (QUOTE QUOTE)) (CAR (CDR E)))
+                     ((EQ (CAR E) (QUOTE COND)) (EVCON (CDR E) A))
+                     ((QUOTE T) (APPLY (CAR E) (EVLIS (CDR E) A) A))))
+              ((QUOTE T) (APPLY (CAR E) (EVLIS (CDR E) A) A)))))) "
                    :expected-stdout "A~%"))
+
   )
